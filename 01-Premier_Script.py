2712b026905d18e0064efcf35295449a83223645
lxkrv <alxkorova@gmail.com>
2020-04-11 17:14:02 +0200

    Premier script en Python

---
+++ 01-Premier_Script.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+print("Hello world")
+
+"""
+ceci est un commentaire
+
+"""
+### ceci est un commentaire
+
+print('L\'envers du décor')
+print("L'envers du décor")
+
+print("L'envers du\ndécors") #passage à la ligne
+print("\tL'envers du décor") #tabulation
