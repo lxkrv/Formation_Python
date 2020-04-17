4892b156ae158043e9032b73ce2cee3e78959890
lxkrv <alxkorova@gmail.com>
2020-04-17 18:36:00 +0200

    fichier index pour les formulaires

--- index.py
+++ index.py
@@ -11,21 +11,21 @@ Limitation du cgi, il va démarrer une requête à chaque actualisation.Sur des 
 On est passé sur du FAST CGI
 """
 
-
-
-
-print("Content-type : text/html; charset=utf-8\n")
-html = """<!Doctype html>
+print("content-type: text/html; charset=utf-8\n")
+    
+html = """<!DOCTYPE html>
 <head>
-    <meta charset="UTF-8">
-    <title>Une page web avec Python</title>
+    <meta charset = utf-8>
+    <title>Ma page web</title>
 </head>
 <body>
-    <h1>Bienvenue</h1>
-    <p>Je suis en Python, avec une page Web avec Python et HTML</p>
-    <p>Blah blah</p>
+    <h1>Page web avec Python CGI</h1>
+
+    <form methode = "post" action = "result.py">
+    <p><input type = "text" name = "username">
+    <input type = "submit" value = "Envoyer"></p>
+    </form>
 </body>
 </html>
 """
 print(html)
-print("<h1>Hello</h1>")
