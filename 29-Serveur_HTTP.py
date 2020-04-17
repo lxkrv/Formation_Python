119c70718431258cad5b7e5f2470bfdc49015b56
lxkrv <alxkorova@gmail.com>
2020-04-17 18:35:15 +0200

    Serveur en Python

--- 29-Serveur_HTTP.py
+++ 29-Serveur_HTTP.py
@@ -1,6 +1,24 @@
 #coding:utf-8
 import os
-import time #--->objet du cours
 # for windows
 os.system('cls')
 #os.system("ls")
+
+import http.server
+import socketserver
+
+port = 80
+address = ("",port)
+
+server = http.server.HTTPServer
+
+handler = http.server.CGIHTTPRequestHandler
+handler.cgi_directories = ["/"]
+
+httpd = server(address, handler)
+
+
+print(f"Serveur démarré sur le PORT : {port}")
+httpd.serve_forever()
+
+
